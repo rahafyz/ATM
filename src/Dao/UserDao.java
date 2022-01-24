684dac5f62e61ac4aaf7ee8fa07ea09da68da2e0
Raziyeh Fayyaz <60230053+rahafyz@users.noreply.github.com>
2022-01-25 00:57:16 +0330

    Update UserDao.java

--- src/Dao/UserDao.java
+++ src/Dao/UserDao.java
@@ -29,7 +29,8 @@ public class UserDao implements Dao<User>{
     @Override
     public Optional get(Integer id) {
         Optional<User> findUser = Optional.empty();
-        try (Connection connection = getConnection();
+        Connection connection = getConnection();
+        try (
              PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID);){
              preparedStatement.setInt(1, id);
              ResultSet resultSet = preparedStatement.executeQuery();
@@ -49,7 +50,8 @@ public class UserDao implements Dao<User>{
     @Override
     public List getAll() {
         List<User> users = new ArrayList<>();
-        try (Connection connection = getConnection();
+        Connection connection = getConnection();
+        try (
             PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL);
             ResultSet resultSet = preparedStatement.executeQuery();){
             while (resultSet.next()){
@@ -67,7 +69,8 @@ public class UserDao implements Dao<User>{
 
     @Override
     public void save(User user) {
-        try (Connection connection = getConnection();
+        Connection connection = getConnection();
+        try (
              PreparedStatement preparedStatement = connection.prepareStatement(INSERT, RETURN_GENERATED_KEYS);){
             preparedStatement.setString(1, user.getName());
             preparedStatement.setString(2, user.getFamily());
@@ -90,7 +93,8 @@ public class UserDao implements Dao<User>{
 
     @Override
     public void update(User user, String[] params) {
-        try (Connection connection = getConnection();
+        Connection connection = getConnection();
+        try (
              PreparedStatement preparedStatement = connection.prepareStatement(UPDATE);){
             preparedStatement.setString(1, params[0]);
             preparedStatement.setString(2, params[1]);
@@ -109,7 +113,8 @@ public class UserDao implements Dao<User>{
 
     @Override
     public void delete(User user) {
-        try (Connection connection = getConnection();
+        Connection connection = getConnection();
+        try (
              PreparedStatement preparedStatement = connection.prepareStatement(DELETE);){
             preparedStatement.setInt(1, user.getId());
             preparedStatement.executeUpdate();
@@ -123,7 +128,7 @@ public class UserDao implements Dao<User>{
 
 
     private Connection getConnection(){
-        Connection conn = DbConnector.getInstance().getConnection();
+        Connection conn = DbConnector.getConnection();
         return conn;
     }
 
